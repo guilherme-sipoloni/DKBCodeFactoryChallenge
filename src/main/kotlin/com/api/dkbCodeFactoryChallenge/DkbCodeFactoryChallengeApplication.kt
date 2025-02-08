ff18769be31c56c7f71b0a27beffe6f0d8b83f50
guilherme-sipoloni <guilhermesipoloni@gmail.com>
2025-02-08 15:53:04 +0100

    init

---
+++ src/main/kotlin/com/api/dkbCodeFactoryChallenge/DkbCodeFactoryChallengeApplication.kt
@@ -0,0 +1,11 @@
+package com.api.dkbCodeFactoryChallenge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DkbCodeFactoryChallengeApplication
+
+fun main(args: Array<String>) {
+	runApplication<DkbCodeFactoryChallengeApplication>(*args)
+}
---
+++ src/test/kotlin/com/api/dkbCodeFactoryChallenge/DkbCodeFactoryChallengeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.dkbCodeFactoryChallenge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DkbCodeFactoryChallengeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
